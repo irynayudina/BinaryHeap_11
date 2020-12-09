811ef038170b5d17cf7726d271f40a41f32a2961
Iryna Yudina <yudinaira4444@gmail.com>
2020-12-09 11:43:12 +0200

    Добавьте файлы проекта.

---
+++ BinaryTree.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryHeap_11
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node()
+        {
+
+        }
+        public Node(int data)
+        {
+            this.Data = data;
+
+        }
+        public override string ToString()
+        {
+            return $"Data: {Data}";
+        }
+        public override bool Equals(object obj)
+        {
+            Node oj = obj as Node;
+            if (oj != null && oj.Data == this.Data)
+            {
+                if (oj.Left == null && this.Left == null && oj.Right == null && this.Right == null)
+                {
+                    return true;
+                }
+                if (oj.Left == null && this.Left == null)
+                {
+                    if (oj.Right != null && this.Right != null)
+                    {
+                        if (oj.Right.Data == this.Right.Data)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                if (oj.Right == null && this.Right == null)
+                {
+                    if (oj.Left != null && this.Left != null)
+                    {
+                        if (oj.Left.Data == this.Left.Data)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                if (oj.Left != null && this.Left != null && oj.Right != null && this.Right != null)
+                {
+                    if (oj.Left.Data == this.Left.Data && oj.Right.Data == this.Right.Data)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Node n1, Node n2)
+        {
+            if (object.ReferenceEquals(n1, null))
+            {
+                return object.ReferenceEquals(n2, null);
+            }
+            return n1.Equals(n2);
+        }
+        public static bool operator !=(Node n1, Node n2)
+        {
+            if (object.ReferenceEquals(n1, null))
+            {
+                return !object.ReferenceEquals(n2, null);
+            }
+            return !n1.Equals(n2);
+        }
+    }
+    public class BinaryTree
+    {
+        private Node _root;
+        public BinaryTree()
+        {
+            _root = null;
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////INSERT////////////////////////////     
+        public void Insert(int data)
+        {
+            // 1. If the tree is empty, return a new, single node 
+            if (_root == null)
+            {
+                _root = new Node(data);
+                return;
+            }
+            // 2. Otherwise, recur down the tree 
+            InsertRec(_root, new Node(data));
+        }
+        private void InsertRec(Node root, Node newNode)
+        {
+            //if (root == null)
+            //    root = newNode;
+            //if (root.Left == null)
+            //{
+            //    root.Left = newNode;
+            //}
+            //if (root.Left != null)
+            //{
+            //    root.Right = newNode;
+            //}
+            if (newNode.Data < root.Data)
+            {
+                if (root.Left == null)
+                    root.Left = newNode;
+                else
+                    InsertRec(root.Left, newNode);
+            }
+            else
+            {
+                if (root.Right == null)
+                    root.Right = newNode;
+                else
+                    InsertRec(root.Right, newNode);
+            }
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////REMOVE//////////////////////
+        public void Remove(int value)
+        {
+            this._root = Remove(this._root, value);
+        }
+        private Node Remove(Node parent, int key)
+        {
+            if (parent == null) return parent;
+
+            if (key < parent.Data) parent.Left = Remove(parent.Left, key);
+            else if (key > parent.Data)
+                parent.Right = Remove(parent.Right, key);
+            // if value is same as parent's value, then this is the node to be deleted  
+            else
+            {
+                // node with only one child or no child  
+                if (parent.Left == null)
+                    return parent.Right;
+                else if (parent.Right == null)
+                    return parent.Left;
+                // node with two children: Get the inorder successor (smallest in the right subtree)  
+                parent.Data = MinValue(parent.Right);
+                // Delete the inorder successor  
+                parent.Right = Remove(parent.Right, parent.Data);
+            }
+            return parent;
+        }
+        private int MinValue(Node node)
+        {
+            int minv = node.Data;
+            while (node.Left != null)
+            {
+                minv = node.Left.Data;
+                node = node.Left;
+            }
+            return minv;
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////PRINT//////////////////////
+        public Node Root { get { return _root; } set { _root = value; } }
+        public void Print()
+        {
+            Root.Print();
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////FIND BY KEY STRAIGHT preORDER//////
+        public void TraversePreOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                Console.Write(parent.Data + " ");
+                TraversePreOrder(parent.Left);
+                TraversePreOrder(parent.Right);
+            }
+
+        }
+        //****************************************************************************************************
+        public int size(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return (size(node.Left) + 1 + size(node.Right));
+            }
+        }
+        int s = 0;
+        //private void makeSize()
+        //{
+        //    s = size(this._root);
+        //}
+        public readonly Node[] ForWideTraverse = new Node[1];
+        //public void TraverseWide()
+        //{
+        //    if(ForWideTraverse.Length - s < 1)
+        //    {
+        //        Array.Resize(ref ForWideTraverse, ForWideTraverse.Length + 1);
+        //    }
+
+        //    s++;
+        //}
+        //***********************************************************************************************
+        public Node Find(int value)
+        {
+            if (this.Find(value, this._root) == null)
+            {
+                Console.WriteLine("Node is not found");
+            }
+            return this.Find(value, this._root);
+        }
+        private Node Find(int value, Node parent)
+        {
+            if (parent != null)
+            {
+                if (value == parent.Data) return parent;
+                if (value < parent.Data)
+                    return Find(value, parent.Left);
+                else
+                    return Find(value, parent.Right);
+            }
+
+            return null;
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////FIND BY KEY BACKWARDS postORDER/////
+        public void TraversePostOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                TraversePostOrder(parent.Left);
+                TraversePostOrder(parent.Right);
+                Console.Write(parent.Data + " ");
+            }
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////FIND MIN//////////////////////
+        public Node FindMin()
+        {
+            return Min(_root);
+        }
+        private Node Min(Node node)
+        {
+            int minv;
+            if (node != null)
+            {
+                minv = node.Data;
+
+                while (node.Left != null)
+                {
+                    minv = node.Left.Data;
+                    node = node.Left;
+                }
+            }
+            return node;
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////FIND MAX///////////////////////
+        public Node FindMax()
+        {
+            return Max(_root);
+        }
+        private Node Max(Node node)
+        {
+            int minv;
+            if (node != null)
+            {
+                minv = node.Data;
+
+                while (node.Right != null)
+                {
+                    minv = node.Right.Data;
+                    node = node.Right;
+                }
+            }
+            return node;
+        }
+        //////////////////////////////////////////////////////////////////GET LEVEL OF NODE////////////////////////////////////////
+        private int getLevelUtil(Node root, Node searched, int level)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root == searched)
+            {
+                return level;
+            }
+
+            int downlevel = getLevelUtil(root.Left, searched, level + 1);
+            if (downlevel != 0)
+            {
+                return downlevel;
+            }
+
+            downlevel = getLevelUtil(root.Right, searched, level + 1);
+            return downlevel;
+        }
+        public int getLevel(Node node, Node searched)
+        {
+            return getLevelUtil(node, searched, 1);
+        }
+    }
+    public static class BTreePrinter
+    {
+        class NodeInfo
+        {
+            public Node NoDe;
+            public string Text;
+            public int StartPos;
+            public int Size { get { return Text.Length; } }
+            public int EndPos { get { return StartPos + Size; } set { StartPos = value - Size; } }
+            public NodeInfo Parent, Left, Right;
+        }
+
+        public static void Print(this Node root, string textFormat = "0", int spacing = 1, int topMargin = 2, int leftMargin = 2)
+        {
+            if (root == null) return;
+            int rootTop = Console.CursorTop + topMargin;
+            var last = new SingleLinkedList<NodeInfo>();
+            var next = root;
+            for (int level = 0; next != null; level++)
+            {
+                var item = new NodeInfo { NoDe = next, Text = next.Data.ToString(textFormat) };
+                if (level < last.Count())
+                {
+                    item.StartPos = last[level].data.EndPos + spacing;
+                    last[level].data = item;
+                }
+                else
+                {
+                    item.StartPos = leftMargin;
+                    last.InsertLast(last, item);//Add(item)
+                }
+                if (level > 0)
+                {
+                    item.Parent = last[level - 1].data;
+                    if (next == item.Parent.NoDe.Left)
+                    {
+                        item.Parent.Left = item;
+                        item.EndPos = Math.Max(item.EndPos, item.Parent.StartPos - 1);
+                    }
+                    else
+                    {
+                        item.Parent.Right = item;
+                        item.StartPos = Math.Max(item.StartPos, item.Parent.EndPos + 1);
+                    }
+                }
+                next = next.Left ?? next.Right;
+                for (; next == null; item = item.Parent)
+                {
+                    int top = rootTop + 2 * level;
+                    Print(item.Text, top, item.StartPos);
+                    if (item.Left != null)
+                    {
+                        Print("/", top + 1, item.Left.EndPos);
+                        Print("_", top, item.Left.EndPos + 1, item.StartPos);
+                    }
+                    if (item.Right != null)
+                    {
+                        Print("_", top, item.EndPos, item.Right.StartPos - 1);
+                        Print("\\", top + 1, item.Right.StartPos - 1);
+                    }
+                    if (--level < 0) break;
+                    if (item == item.Parent.Left)
+                    {
+                        item.Parent.StartPos = item.EndPos + 1;
+                        next = item.Parent.NoDe.Right;
+                    }
+                    else
+                    {
+                        if (item.Parent.Left == null)
+                            item.Parent.EndPos = item.StartPos - 1;
+                        else
+                            item.Parent.StartPos += (item.StartPos - 1 - item.Parent.EndPos) / 2;
+                    }
+                }
+            }
+            Console.SetCursorPosition(0, rootTop + 2 * last.Count() - 1);
+        }
+
+        private static void Print(string s, int top, int left, int right = -1)
+        {
+            Console.SetCursorPosition(left, top);
+            if (right < 0) right = left + s.Length;
+            while (Console.CursorLeft < right) Console.Write(s);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+
+namespace BinaryHeap_11
+{
+    class BinaryHeapMax
+    {
+        private int[] arrayOfelementsHEAP = new int[1];
+        private int heapsize = 0;
+        public int[] arraySorted = new int[1];
+        //BinaryTree tree = new BinaryTree();
+
+
+
+        public void PrintHeap()
+        {
+            Console.WriteLine("Array:");
+            foreach (int i in arrayOfelementsHEAP)
+            {
+                Console.Write(i + "   ");
+            }
+            Console.WriteLine();
+            //Console.WriteLine("Heap:");
+            //for(int i =0; i <=heapsize; i++)
+            //{
+            //    int line = 0;
+            //    line += Convert.ToInt32(Math.Pow(2, i));
+            //    if(i == line)
+            //    {
+
+            //    }
+            //}
+        }
+
+
+
+        public void insert(int e)
+        {
+            insert_arr(e);
+        }
+        private void insert_arr(int elem)
+        {
+            if ((arrayOfelementsHEAP.Length - heapsize) < 2)
+            {
+                Array.Resize(ref arrayOfelementsHEAP, arrayOfelementsHEAP.Length + 1);
+            }
+            heapsize++;
+            arrayOfelementsHEAP[heapsize] = elem;
+            HeapifyBottomToTop(heapsize);
+        }
+
+
+
+
+        public int maximum()
+        {
+            if (heapsize == 0)
+            {
+                Console.WriteLine("Heap is empty");
+                return 0;
+            }
+            Console.WriteLine("Head of the Heap is: " + arrayOfelementsHEAP[1]);
+            return arrayOfelementsHEAP[1];
+        }
+
+
+
+
+        public int extractMax()
+        {
+            if (heapsize == 0)
+            {
+                Console.WriteLine("Heap is empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("Head of the Heap is: " + arrayOfelementsHEAP[1]);
+                Console.WriteLine("Extracting it now...");
+                int extractedValue = arrayOfelementsHEAP[1];
+                arrayOfelementsHEAP[1] = arrayOfelementsHEAP[heapsize]; //Replacing with last element of the array  
+                heapsize--;
+                HeapifyTopToBottom(1);
+                Array.Resize(ref arrayOfelementsHEAP, heapsize+1);
+                return extractedValue;
+            }
+        }
+
+
+
+
+
+        public void increase(int x, int k)
+        {
+            int ind = 0;
+            if (k >= x)
+            {
+                for (int i = 0; i <= heapsize; i++)
+                {
+                    if (arrayOfelementsHEAP[i] == x)
+                    {
+                        ind = i;
+                    }
+                }
+                arrayOfelementsHEAP[ind] = k;
+                //PrintHeap();
+                HeapifyBottomToTop(ind);
+            }
+
+        }
+
+
+
+        public void HeapSort()
+        {
+            Array.Resize(ref arraySorted, heapsize+1);
+            int size = heapsize;
+            for(int i=0; i<=size; i++)
+            {
+                arraySorted[size - i] = this.extractMax();
+                if (i == size)
+                {
+                    arraySorted[size - i] = 0;
+                }
+            }
+            Console.WriteLine("Array:");
+            foreach (int i in arraySorted)
+            {
+                Console.Write(i + "   ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        private void HeapifyBottomToTop(int index)
+        {
+            int parent = index / 2;
+            // We are at root of the tree. Hence no more Heapifying is required.  
+            if (index <= 1)
+            {
+                return;
+            }
+            // If Current value is smaller than its parent, then we need to swap  
+            if (arrayOfelementsHEAP[index] > arrayOfelementsHEAP[parent])
+            {
+                int tmp = arrayOfelementsHEAP[index];
+                arrayOfelementsHEAP[index] = arrayOfelementsHEAP[parent];
+                arrayOfelementsHEAP[parent] = tmp;
+            }
+            HeapifyBottomToTop(parent);
+        }
+        private void HeapifyTopToBottom(int index)
+        {
+            int left = index * 2;
+            int right = (index * 2) + 1;
+            int biggestChild = 0;
+
+            int sizeOfTree = heapsize;
+            if (sizeOfTree < left)
+            { //If there is no child of this node, then nothing to do. Just return.  
+                return;
+            }
+            else if (sizeOfTree == left)
+            { //If there is only left child of this node, then do a comparison and return.  
+                if (arrayOfelementsHEAP[index] < arrayOfelementsHEAP[left])
+                {
+                    int tmp = arrayOfelementsHEAP[index];
+                    arrayOfelementsHEAP[index] = arrayOfelementsHEAP[left];
+                    arrayOfelementsHEAP[left] = tmp;
+                }
+                return;
+            }
+            else
+            { //If both children are there  
+                if (arrayOfelementsHEAP[left] > arrayOfelementsHEAP[right])
+                { //Find out the smallest child  
+                    biggestChild = left;
+                }
+                else
+                {
+                    biggestChild = right;
+                }
+                if (arrayOfelementsHEAP[index] < arrayOfelementsHEAP[biggestChild])
+                { //If Parent is greater than smallest child, then swap  
+                    int tmp = arrayOfelementsHEAP[index];
+                    arrayOfelementsHEAP[index] = arrayOfelementsHEAP[biggestChild];
+                    arrayOfelementsHEAP[biggestChild] = tmp;
+                }
+            }
+            HeapifyTopToBottom(biggestChild);
+        }
+
+        
+       
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int a = 0, b = 0, c = 0;
+            BinaryHeapMax heap = new BinaryHeapMax();
+            while (a != 7)
+            {
+                Console.WriteLine("choose action: \n1-insert\n2-maximun\n3-extract max\n4-increase key\n5-heap sort\n6-print\n7-exit");
+                line = Console.ReadLine();
+                a = int.Parse(line);
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("enter value: ");
+                        line = Console.ReadLine();
+                        b = int.Parse(line);
+                        heap.insert(b);
+                        break;
+                    case 2:
+                        int m = heap.maximum();
+                        break;
+                    case 3:
+                        int m1 = heap.extractMax();
+                        break;
+                    case 4:
+                        Console.WriteLine("enter value x: ");
+                        line = Console.ReadLine();
+                        b = int.Parse(line);
+                        Console.WriteLine("enter value y: ");
+                        line = Console.ReadLine();
+                        c = int.Parse(line);
+                        heap.increase(b, c);
+                        break;
+                    case 5:
+                        heap.HeapSort();
+                        break;
+                    case 6:
+                        heap.PrintHeap();
+                        break;
+                }
+            }
+        }
+    }
+}
+
